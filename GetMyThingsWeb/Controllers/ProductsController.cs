1050ac1f4364a277b50c4166a4d1292881510d3a
Pradip <Pradip@DESKTOP-7D2H6S8>
2025-01-16 19:54:54 +0530

    7. Read and Display the Product

---
+++ GetMyThingsWeb/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using GetMyThingsWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GetMyThingsWeb.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.db = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            var products = db.Products.OrderByDescending(m => m.Id).ToList();
+
+            return View(products);
+        }
+    }
+}
